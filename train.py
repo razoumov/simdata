d45883fdb7de79bf6dd9ef44628cbd307a059353
Alex Razoumov <alex.razoumov@westdri.ca>
2026-04-22 19:33:25 -0700

    convergence plateaus and then oscillates; trying diff techniques to converge

--- train.py
+++ train.py
@@ -69,11 +69,11 @@ match modelName:
         # tx = optax.adamw(learning_rate=learning_rate, weight_decay=weight_decay)
         # optimizer = nnx.Optimizer(model, tx, wrt=nnx.Param)
         # ---
-        learningRateSchedule = optax.cosine_decay_schedule(init_value=1e-3, decay_steps=2000, alpha=0.1)
+        learningRateSchedule = optax.cosine_decay_schedule(init_value=1e-3*2, decay_steps=2000, alpha=0.01)
         tx = optax.chain(
             optax.clip_by_global_norm(1.0),   # clip gradients to a max global norm of 1.0
             # optax.adam(learningRateSchedule)
-            optax.adamw(learning_rate=learningRateSchedule, weight_decay=1e-4)
+            optax.adamw(learning_rate=learningRateSchedule, weight_decay=0)   # default decay 1e-4
         )
         optimizer = nnx.Optimizer(model, tx, wrt=nnx.Param)
 
